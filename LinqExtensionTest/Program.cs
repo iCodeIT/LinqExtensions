d24a7bf4578533f7c1853d73cb45b0656287bd93
Michael Lund <ml@icodeit.dk>
2015-06-29 22:40:52 +0200

    Added debug dump methods

---
+++ LinqExtensionTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+using LinqExtensions;
+
+namespace LinqExtensionTest
+{
+  // See output in debug output
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var numbers = new[] { 2, 1, 5, 6, 9, 3, 7, 8, 4 };
+      var evenNumbersAbove3EnumerateAtOnce = numbers.Dump(true, "numbers")
+        .Where(x => x % 2 == 0).Dump(true, "Even")
+        .Where(x => x > 3).Dump(true, "gt3")
+        .Distinct().Dump(true, "Distinct")
+        .OrderBy(x => x).Dump(true, "Ordered");
+
+      Debug.WriteLine("------------------------------");
+
+      var evenNumbersAbove3LazyEval = numbers.Dump(false, "numbers")
+        .Where(x => x % 2 == 0).Dump(false, "Even")
+        .Where(x => x > 3).Dump(false, "gt3")
+        .Distinct().Dump(false, "Distinct")
+        .OrderBy(x => x).Dump(false, "Ordered").ToList();
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ LinqExtensions/LinqDumper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace LinqExtensions
+{
+  public static class LinqDumper
+  {
+    public static bool ShowWhiteSpace { get; set; }
+
+    public static IEnumerable<T> Dump<T>(this IEnumerable<T> input, bool enumerateAtOnce = true, string dumpName = "")
+    {
+      return Dump(input, item => item != null ? item.ToString() : "(null)", enumerateAtOnce, dumpName);
+    }
+
+    public static IEnumerable<T> Dump<T>(this IEnumerable<T> input, Func<T, string> toString, bool enumerateAtOnce = true, string dumpName = "")
+    {
+      if(enumerateAtOnce)
+        return InternDump(input, toString, dumpName).ToList();
+      return InternDump(input, toString, dumpName);
+    }
+
+    private static IEnumerable<T> InternDump<T>(this IEnumerable<T> input, Func<T, string> toString, string dumpName)
+    {
+      Debug.WriteLine("Dump:" + dumpName);
+      foreach (var item in input)
+      {
+        Debug.WriteLine(dumpName + ": " + (ShowWhiteSpace ? '[' + toString(item) + ']' : toString(item)));
+        yield return item;
+      }
+    }
+  }
+}
