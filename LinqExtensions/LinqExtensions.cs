ca2fcf075d25f386ac6c588da52eae4955f1e2c5
Michael Lund <ml@icodeit.dk>
2016-08-24 08:47:02 +0200

    Added In method and updated to C#6 syntax

--- LinqExtensions/LinqExtensions.cs
+++ LinqExtensions/LinqExtensions.cs
@@ -1,29 +1,21 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace LinqExtensions
 {
   public static class LinqExtensions
   {
-    public static bool None<TSource>(this IEnumerable<TSource> source)
-    {
-      return !source.Any();
-    }
+    public static bool None<TSource>(this IEnumerable<TSource> source) => !source.Any(); 
 
-    public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
-    {
-      return !source.Any(predicate);
-    }
+    public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate) => !source.Any(predicate);
 
     public static bool ContainsExactly<TSource>(this IEnumerable<TSource> source, int count)
     {
       if (count < 0)
-        throw new ArgumentOutOfRangeException("count");
+        throw new ArgumentOutOfRangeException(nameof(count));
       if (source == null)
-        throw new ArgumentNullException("source");
+        throw new ArgumentNullException(nameof(source));
       using (var e = source.GetEnumerator())
       {
         for (int i = 0; i < count; i++)
@@ -36,38 +28,29 @@ namespace LinqExtensions
     }
 
     public static bool ContainsExactly<TSource>(this IEnumerable<TSource> source, int count, Func<TSource, bool> predicate)
-    {
-      return source.Where(predicate).ContainsExactly(count);
-    }
+      => source.Where(predicate).ContainsExactly(count);
 
     public static bool ContainsMoreThan<TSource>(this IEnumerable<TSource> source, int count)
     {
       if (count < 0)
-        throw new ArgumentOutOfRangeException("elementCount");
+        throw new ArgumentOutOfRangeException(nameof(count));
       if (source == null)
-        throw new ArgumentNullException("source");
+        throw new ArgumentNullException(nameof(source));
       return source.Skip(count).Any();
     }
 
-    public static bool ContainsMoreThan<TSource>(this IEnumerable<TSource> source, int count, Func<TSource, bool> predicate)
-    {
-      return source.Where(predicate).ContainsMoreThan(count);
-    }
+   public static bool ContainsMoreThan<TSource>(this IEnumerable<TSource> source, int count, Func<TSource, bool> predicate) =>
+      source.Where(predicate).ContainsMoreThan(count);
 
-    public static IEnumerable<T> NullToEmpty<T>(this IEnumerable<T> source)
-    {
-      return source ?? EmptyEnumerable<T>.Instance;
-    }
+    public static IEnumerable<T> NullToEmpty<T>(this IEnumerable<T> source) => source ?? EmptyEnumerable<T>.Instance;
 
-    internal class EmptyEnumerable<TElement>
+    private static class EmptyEnumerable<TElement>
     {
       static TElement[] instance;
 
-      public static IEnumerable<TElement> Instance
-      {
-        get { return instance ?? (instance = new TElement[0]); }
+      public static IEnumerable<TElement> Instance => instance ?? (instance = new TElement[0]);
       }
-    }
 
+    public static bool In<T>(this T element, IEnumerable<T> collection) => collection.Contains(element);
   }
 }
