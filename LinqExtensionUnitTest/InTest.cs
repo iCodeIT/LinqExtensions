16d05127d7215b25bfeeedea8ad511218b0490d2
Michael Lund <ml@icodeit.dk>
2019-05-14 21:09:13 +0200

    Added some simple unit tests

---
+++ LinqExtensionUnitTest/InTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LinqExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinqExtensionUnitTest
+{
+  [TestClass]
+  public class InTest
+  {
+    [TestMethod]
+    public void Should_ReturnFalse_WhenObjectIsNotInCollection()
+    {
+      Assert.IsFalse(1.In(new[] { 2, 3, 4 }));
+    }
+
+    [TestMethod]
+    public void Should_ReturnTrue_WhenObjectIsInCollection()
+    {
+      Assert.IsTrue(1.In(new[] { 1, 2, 3, 4 }));
+    }
+  }
+}
---
+++ LinqExtensionUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LinqExtensionUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LinqExtensionUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b73969fe-cc44-4918-8b1b-a492a382d00f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
